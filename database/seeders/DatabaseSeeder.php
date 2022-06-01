0f1f0aeeb41dc1286ba558262d3a314acce51634
Esteban Sevilla <esteban@perspective.com.mx>
2022-06-01 17:06:07 -0500

    Run seeder Config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\Config;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +17,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(1)->create();
+
+        $this->call(
+            Config::class,
+        );
     }
 }
